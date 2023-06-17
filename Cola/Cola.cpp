05dc21f7cc2a5e225cf2522d7de32c702e4a6438
Feketerigo50 <kevinandjack50@gmail.com>
2023-06-17 13:59:37 +0800

    Cola

---
+++ Cola/Cola.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    while( cin >> n )
+        cout << int( n * 1.5 ) << endl;
+
+    return 0;
+}
+
+
+// 其實很簡單 就*1.5取整 規律
