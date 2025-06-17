92c346a179993285b50a1dbf3bd47668ad74efc0
Feketerigo50 <kevinandjack50@gmail.com>
2025-06-17 18:52:46 +0800

    0617

---
+++ Validate BST/code.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return validate(root, LONG_MIN, LONG_MAX);
+    }
+    bool validate(TreeNode* root, long min_val, long max_val) {
+        if (root == NULL) return true;
+
+        if (root->val <= min_val) return false;
+        if (root->val >= max_val) return false;
+
+        return validate(root->left, min_val, root->val) && validate(root->right, root->val, max_val);
+    }
+};
